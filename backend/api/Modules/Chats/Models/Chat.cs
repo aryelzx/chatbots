3d71201f21a3a9af265463ea22e644df9897c4d9
aryelzx <arielcearamor102@gmail.com>
2025-07-27 13:13:43 -0300

    refactor(chats): Clean up Chats and Mensagem classes by removing unused properties and improving structure

--- backend/api/Modules/Chats/Models/Chat.cs
+++ backend/api/Modules/Chats/Models/Chat.cs
@@ -2,25 +2,38 @@ namespace ModularApi.Modules.Users.Models
 {
     public class Chats
     {
-        public int Id { get; set; }
-        public string Message { get; set; }
-        public DateTime Timestamp { get; set; }
-        public int UserId { get; set; }
+       public int Id { get; set; }
 
-        // Navigation property
-        public User User { get; set; }
+        public required string Context { get; set; } 
+
+        public required string Nome { get; set; } 
+
+        public required string Modelo { get; set; } 
+
+        public string? Descricao { get; set; } 
+        public required string Status { get; set; } // 'A' - Active, 'I' - Inactive
+
+        public int UserId { get; set; }
     }
     public class Mensagem
     {
-        public int Id { get; set; }
-        public string Conteudo { get; set; }
-        public DateTime DataHora { get; set; }
-        public int ChatId { get; set; }
+        public int Id { get; set; } 
+        public int ChatId { get; set; } 
 
-        // Navigation property
-        public Chats Chat { get; set; }
         public int UserId { get; set; }
-        // Navigation property
-        public User User { get; set; }
+
+        public string MensagemTexto { get; set; }
+
+        public string? PromptInputText { get; set; } 
+
+        public string Tipo { get; set; }
+
+        public string? PromptContext { get; set; }
+
+        public string? PromptRole { get; set; }
+
+        public string? PromptModelo { get; set; }
+
+        public string SendBy { get; set; }
     }
 }
