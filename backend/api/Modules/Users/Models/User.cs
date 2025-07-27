013cedf0dcbd62f9c2ee58cee0584ed3bee5a818
aryelzx <arielcearamor102@gmail.com>
2025-07-27 13:07:37 -0300

    feat(users): Add Role and Cpf properties to User model

--- backend/api/Modules/Users/Models/User.cs
+++ backend/api/Modules/Users/Models/User.cs
@@ -2,8 +2,16 @@ namespace ModularApi.Modules.Users.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
+       public int Id { get; set; }
+
+        public string Role { get; set; } // 'A' = Admin, 'U' = User
+
+        public string? Email { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Cpf { get; set; } 
+
+        public string Senha { get; set; } 
     }
 }
