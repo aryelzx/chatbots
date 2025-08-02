d3f75433758d75d7c428c607dc1cc2be2dcb9d8a
aryelzx <arielcearamor102@gmail.com>
2025-08-02 15:01:20 -0300

    fix(errorHandler): simplify error handling logic by removing multiple error checks

--- frontend/front/src/shared/api/errorHandler.ts
+++ frontend/front/src/shared/api/errorHandler.ts
@@ -3,13 +3,8 @@ import { toast } from "react-hot-toast";
 const errorHandler = (error: any, context?: string) => {
 	let err: string;
 
-	const hasMultipleErrors = Array.isArray(error.response?.data.message);
-	if (hasMultipleErrors) {
-		err = `${
-			error.response.data.statusCode
-		}: ${error.response.data.message.join(" / ")}`;
-	} else if (error.response.data) {
-		err = `${error.response.data.statusCode}: ${error.response.data.message}`;
+	if (error.message) {
+		err = `${error.message}`;
 	} else {
 		err = "500: Erro interno do servidor";
 	}
