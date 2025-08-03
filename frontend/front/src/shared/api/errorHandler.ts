3e068de58104b20eb447291924e220744a3098cb
aryelzx <arielcearamor102@gmail.com>
2025-08-03 14:40:46 -0300

    fix(errorHandler): improve error handling and message formatting

--- frontend/front/src/shared/api/errorHandler.ts
+++ frontend/front/src/shared/api/errorHandler.ts
@@ -1,16 +1,21 @@
 import { toast } from "react-hot-toast";
+import type { AxiosError } from "axios";
 
-const errorHandler = (error: any, context?: string) => {
-	let err: string;
+const errorHandler = (error: unknown, context?: string) => {
+  let errMsg = "500: Erro interno do servidor";
+  let statusCode: number | null = null;
 
-	if (error.message) {
-		err = `${error.message}`;
-	} else {
-		err = "500: Erro interno do servidor";
-	}
+  // Se for um erro Axios
+  if ((error as AxiosError).isAxiosError) {
+    const axiosError = error as AxiosError;
+    statusCode = axiosError.response?.status ?? null;
+    errMsg = axiosError.response?.data?.message ?? axiosError.message;
+  } else if (error instanceof Error) {
+    errMsg = error.message;
+  }
 
-	console.error(`${context?.toUpperCase()} - ${err}`);
-	toast.error(err);
+  console.error(`${context?.toUpperCase() ?? "ERRO"} - [${statusCode ?? "??"}] ${errMsg}`);
+  toast.error(`[${statusCode ?? "??"}] - ${errMsg}`);
 };
 
 export { errorHandler };
