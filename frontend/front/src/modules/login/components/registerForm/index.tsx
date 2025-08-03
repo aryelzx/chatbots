6992b023b584ecc556f5e2653701156b28bd27be
aryelzx <arielcearamor102@gmail.com>
2025-08-03 14:54:59 -0300

    feat(registerForm): add password visibility toggle to registration form

--- frontend/front/src/modules/login/components/registerForm/index.tsx
+++ frontend/front/src/modules/login/components/registerForm/index.tsx
@@ -20,6 +20,7 @@ import { errorHandler } from "@/shared/api/errorHandler";
 import { useState } from "react";
 import { LoaderComponent } from "@/shared/components/loader";
 import toast from "react-hot-toast";
+import { Eye, EyeClosed } from "lucide-react";
 
 type Props = {
 	callBack: (value: string) => void;
@@ -27,6 +28,7 @@ type Props = {
 
 function RegisterUserFormComponent({ callBack }: Props) {
 	const [loading, setLoading] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 
 	const form = useForm<registerUserSchemaType>({
 		resolver: zodResolver(registerUserSchema),
@@ -133,12 +135,40 @@ function RegisterUserFormComponent({ callBack }: Props) {
 							<FormItem>
 								<FormLabel>Senha</FormLabel>
 								<FormControl>
-									<Input
-										type="password"
-										placeholder="Digite a senha"
-										{...field}
-										className="h-11 rounded-md"
-									/>
+									<div className="relative w-full">
+										<Input
+											type={
+												showPassword
+													? "text"
+													: "password"
+											}
+											placeholder="Digite a senha"
+											{...field}
+											className="h-11 rounded-md pr-10"
+										/>
+										{form.watch("senha") && (
+											<button
+												type="button"
+												onClick={() =>
+													setShowPassword(
+														!showPassword
+													)
+												}
+												className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 hover:text-gray-700 transition"
+												aria-label={
+													showPassword
+														? "Esconder senha"
+														: "Mostrar senha"
+												}
+											>
+												{showPassword ? (
+													<EyeClosed />
+												) : (
+													<Eye />
+												)}
+											</button>
+										)}
+									</div>
 								</FormControl>
 								<FormMessage />
 							</FormItem>
